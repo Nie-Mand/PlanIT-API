c1201145983100074d28b667fff874ce8353f2ec
Nie-Mand <chill.out.x19@gmail.com>
2022-01-15 03:29:43 +0100

    getting started

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { config } from 'dotenv'
+config()
+
+import main from './main'
+main()
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+
+
+export default () => {
+    console.log('Hello World!');
+    console.log('hello', process.env.HELLO);
+    
+    
+}
